282ba9f1c346fec3e58c6a5ff766d0cef35d78c8
jshdplo <184445792+jshdplo@users.noreply.github.com>
2024-11-14 11:45:57 -0600

    snow update

--- js/Snow.js
+++ js/Snow.js
@@ -4,10 +4,10 @@ export const Snow = () => {
   const canvas = document.getElementById('snow-canvas');
   const ctx = canvas.getContext('2d');
 
-  const snowflakes = Array.from({ length: 50 }).map(() => ({
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
-    radius: Math.random() * 3 + 1,
+  const snowflakes = Array.from({ length: 25 }).map(() => ({
+    x: Math.random() * window.innerWidth,
+    y: Math.random() * window.innerHeight - window.innerHeight,
+    radius: Math.random() * 2 + 1,
     speedY: Math.random() * 1 + .05,
     drift: Math.random() * 2 - 1
   }));
@@ -21,6 +21,9 @@ export const Snow = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     snowflakes.forEach((snowflake) => {
+      const blurAmount = (snowflake.radius / 3) * 1.25;
+      ctx.filter = `blur(${blurAmount}px)`;
+
       ctx.beginPath();
       ctx.arc(snowflake.x, snowflake.y, snowflake.radius, 0, Math.PI * 2);
       ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
