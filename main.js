cd87b94cf967ee48a560bbbc4f227446db51ae68
jshdplo <184445792+jshdplo@users.noreply.github.com>
2024-11-29 13:44:50 -0600

    start on v2

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 import './css/reset.css';
 import './css/fonts.css';
-import './css/style.css';
-import './css/pages.css';
-
-import { Christmas } from './js/Christmas.js';
-import { Nav } from './js/Nav.js';
+import './css/base.scss';
+import './css/nav.css';
 
 document.addEventListener('DOMContentLoaded', function onDOMLoad() {
   console.log('`¯*¯`·.¸¸.·´¯*¯`·.¸¸.·´¯*¯´\n\n❤️ Have a great day!\n✉️ feedback@joshdplo.me\n\n');
-  Nav();
-  Christmas();
 });
