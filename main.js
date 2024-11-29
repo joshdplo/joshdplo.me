30628ce1acea66813b582f72ed998d36139979fd
jshdplo <184445792+jshdplo@users.noreply.github.com>
2024-11-29 17:38:33 -0600

    nav experimenting

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import './css/reset.css';
 import './css/fonts.css';
 import './css/base.scss';
 import './css/logo.scss';
-import './css/nav.css';
+import './css/nav.scss';
 
 document.addEventListener('DOMContentLoaded', function onDOMLoad() {
   console.log('`¯*¯`·.¸¸.·´¯*¯`·.¸¸.·´¯*¯´\n\n❤️ Have a great day!\n✉️ feedback@joshdplo.me\n\n');
